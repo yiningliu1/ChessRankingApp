80ea82e0d3d3585879167ef644c2a54d99aa2606
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2023-10-03 23:06:35 -0400

    Add files via upload

---
+++ Main_1.java
@@ -0,0 +1,11 @@
+package com.example.chessapp;
+
+import com.example.chessapp.Main;
+
+import java.io.IOException;
+
+public class Main_1 {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
